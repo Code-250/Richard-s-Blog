256518156992280bfde13cf96798e5c3b58c5655
code-250 <richmunye@gmail.com>
2021-01-22 00:46:51 +0200

    created a blog post

--- js/main.js
+++ js/main.js
@@ -9,11 +9,14 @@ $(document).ready(function(){
     })
 
     // owl-carousel for blog
+    
+})
 
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        autoplay:true,
-        autoplayTimeout:2000
-    });
+$(".owl-carousel").owlCarousel({
+     loop:true,
+     autoplay:false,
+     autoplayTimeOut:3000,
+    nav:true,
+    navText:[$(".owl-navigation .owl-nav-prev"),  $( ".owl-navigation .owl-nav-next")]
     
 })
